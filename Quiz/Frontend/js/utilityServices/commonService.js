4b07d3fc6b15e3245df2eb446dc6f0f3d0527236
maid.ostrakovic <maid.ostrakovic@fet.ba>
2022-09-07 22:27:02 +0200

    added new css, change HTMLs and javascript files

--- Quiz/Frontend/js/utilityServices/commonService.js
+++ Quiz/Frontend/js/utilityServices/commonService.js
@@ -15,7 +15,7 @@ function readURL(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
         reader.onload = function (e) {
-            $('#image').attr('src', e.target.result).width(250).height(400);
+            $('#image').attr('src', e.target.result).width(400).height(400);
         };
         reader.readAsDataURL(input.files[0]);
     }
